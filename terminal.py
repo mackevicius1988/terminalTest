41c2857570a3df550787550be8ad903b265802f3
Mantas <mackevicius1988@gmail.com>
2016-05-02 18:04:25 +0300

    test

--- terminal.py
+++ terminal.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     while 1:
         question_data = mainController.get_and_display_question(raspi_id)
-        got_question = question_data['questionMapId'] != ''
+        got_question = question_data is not None and question_data['questionMapId'] != ''
 
         if got_question is False:
             # No question, wait for the minute
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         while 1:
             card_id, answer_id = mainController.wait_for_nfc_input(layout, 60)
             if card_id is False:
-                continue # wait for another input
+                continue  # wait for another input
 
             break
 
